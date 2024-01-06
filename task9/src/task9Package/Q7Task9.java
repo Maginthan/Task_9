9cb9f074178147567e38116fd207ba66d221cbc8
Maginthan <77536587+Maginthan@users.noreply.github.com>
2024-01-06 13:31:18 +0530

    Update Q7Task9.java

--- task9/src/task9Package/Q7Task9.java
+++ task9/src/task9Package/Q7Task9.java
@@ -15,18 +15,6 @@ public class Q7Task9 {
 	    System.out.println("Please enter the second number c: ");
 	    int c = sc.nextInt();
 		
-//		if (a > b && a > c) {
-//			System.out.println(a + " is the largest number");		
-//		}
-//		
-//		else if (b > c && b > a) {
-//			System.out.println(b + " is the largest number");
-//		}
-//		
-//		else {
-//			System.out.println(c + " is the largest number");
-//		}
-		
 		int Max = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
 		
 		System.out.println("The largest number among " + a +" - "+ b +" - "+ c + " is: " + Max);
